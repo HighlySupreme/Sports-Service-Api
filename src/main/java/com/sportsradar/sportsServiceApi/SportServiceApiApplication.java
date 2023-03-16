4de08710293f1817ed0006c22fb89b26bf56b72d
HighlySupreme <gal.1999k@gmail.com>
2023-03-16 17:47:53 +0100

    Initial commit: Created Spring Boot project with Maven
    Created a new Spring Boot project using Spring initializr; Includes initial directory structure; Using Maven and the Spring Boot Starter web dependency

---
+++ src/main/java/com/sportsradar/sportsServiceApi/SportServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.sportsradar.sportsServiceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportsradar/sportsServiceApi/SportServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsradar.sportsServiceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
