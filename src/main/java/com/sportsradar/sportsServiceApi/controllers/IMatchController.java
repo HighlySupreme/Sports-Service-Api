147d9913045879a583f5e3d6d920ce37369bda8f
HighlySupreme <gal.1999k@gmail.com>
2023-03-16 18:08:24 +0100

    Initialized Match Controller and Interface
    Initialized Match Controller and its Interface with prefix of /api/${api.version}/match; added api.version application property for easier version update

---
+++ src/main/java/com/sportsradar/sportsServiceApi/controllers/IMatchController.java
@@ -0,0 +1,10 @@
+package com.sportsradar.sportsServiceApi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/${api.version}/match")
+public interface IMatchController {
+
+
+
+}
---
+++ src/main/java/com/sportsradar/sportsServiceApi/controllers/impl/MatchController.java
@@ -0,0 +1,10 @@
+package com.sportsradar.sportsServiceApi.controllers.impl;
+
+import com.sportsradar.sportsServiceApi.controllers.IMatchController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MatchController implements IMatchController {
+
+
+}
