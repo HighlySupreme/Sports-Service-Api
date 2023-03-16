2117da722cd456f20b5dd1514f3bbb4ae1845863
HighlySupreme <gal.1999k@gmail.com>
2023-03-17 00:22:24 +0100

    Redis removed Fix
    Removed Redis from SportServiceApiApplication

--- src/main/java/com/sportsradar/sportsServiceApi/SportServiceApiApplication.java
+++ src/main/java/com/sportsradar/sportsServiceApi/SportServiceApiApplication.java
@@ -3,9 +3,6 @@ package com.sportsradar.sportsServiceApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootApplication
 @EnableCaching
@@ -15,9 +12,5 @@ public class SportServiceApiApplication {
 		SpringApplication.run(SportServiceApiApplication.class, args);
 	}
 
-	@Bean
-	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-		return RedisCacheManager.builder(redisConnectionFactory).build();
-	}
 
 }
